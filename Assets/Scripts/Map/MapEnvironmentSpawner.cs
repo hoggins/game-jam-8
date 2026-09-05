15a9f1c84517a786bddf9491a3e237b1309d531e
m.gukov <m.gukov@plarium.com>
2026-09-05 12:32:59 +0300

    fix timer

--- Assets/Scripts/Map/MapEnvironmentSpawner.cs
+++ Assets/Scripts/Map/MapEnvironmentSpawner.cs
@@ -70,7 +70,8 @@ namespace Map
 
         var instance = Object.Instantiate(placement.House.prefab, position, Quaternion.identity, _container);
         instance.name = placement.House.name;
-        RegisterRenderers(placementIndex, instance);
+        if (!placement.House.unique)
+          RegisterRenderers(placementIndex, instance);
 
         var destructible = instance.GetComponentInChildren<DestructibleObject>();
         var id = _nextId++;
